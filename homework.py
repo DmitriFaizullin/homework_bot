d406133f6c9acbe3e61b0a0e1782fe94b0124301
Dmitri Faizullin <faizullin-dmitri@mail.ru>
2024-06-27 20:20:17 +0500

    commit1

--- homework.py
+++ homework.py
@@ -1,11 +1,15 @@
-...
+import os
+import requests
+from telebot import TeleBot, types
+from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,7 +24,7 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    return bool(PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID)
 
 
 def send_message(bot, message):
@@ -28,7 +32,8 @@ def send_message(bot, message):
 
 
 def get_api_answer(timestamp):
-    ...
+    api_answer = requests.get(ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
+    return api_answer.json()
 
 
 def check_response(response):
@@ -44,17 +49,21 @@ def parse_status(homework):
 def main():
     """Основная логика работы бота."""
 
-    ...
+    check_tokens()
 
     # Создаем объект класса бота
-    bot = ...
-    timestamp = int(time.time())
+    bot = TeleBot(token=TELEGRAM_TOKEN)
+    timestamp = 0 # int(time.time())
 
     ...
 
     while True:
         try:
-
+            response = get_api_answer(timestamp)
+            check_response(response)
+            homework = response.get('homeworks')[0]
+            message = parse_status(homework)
+            send_message(bot, message)
             ...
 
         except Exception as error:
