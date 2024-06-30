ae492f0ff6aceb98b3cc51a3222a1829988a4060
Dmitri Faizullin <faizullin-dmitri@mail.ru>
2024-06-30 21:57:06 +0500

    commit3

--- exceptions.py
+++ exceptions.py
@@ -1,26 +1,42 @@
 class StatusNotChangedException(Exception):
     def __str__(self):
-        return 'Статус работы не изменился!'
+        return 'статус работы не изменился!'
 
 
 class FormDateException(Exception):
     def __str__(self):
-        return 'Неправильный формат from_date!'
+        return 'неправильный формат from_date!'
 
 
 class NotAuthenticatedException(Exception):
     def __str__(self):
-        return 'Запрос с недействительным или некорректным токеном!'
+        return 'запрос с недействительным или некорректным токеном!'
 
 
 class SendingMessageException(Exception):
     def __str__(self):
-        return 'Сбой при отправке сообщения в Telegram!'
+        return 'сбой при отправке сообщения в Telegram!'
 
 
-class CheckTokensException(Exception):
+class EndpointException(Exception):
+    def __init__(self, error):
+        super().__init__()
+        self.error = error
     def __str__(self):
-        return (
-            'В глобальном окружении отсутствует переменная '
-            '(PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID)!'
-        )
+        return f'эндпоинт недоступен ({self.error})!'
+
+
+class UnexpectedStatusException(Exception):
+    def __init__(self, status):
+        super().__init__()
+        self.status = status
+    def __str__(self):
+        return f'неожиданный статус домажней работы ({self.status})!'
+
+
+class NoKeyException(Exception):
+    def __init__(self, key_name):
+        super().__init__()
+        self.key_name = key_name
+    def __str__(self):
+        return f'отсутствует ключ {self.key_name}!'
--- homework.py
+++ homework.py
@@ -1,11 +1,11 @@
 import os
 import requests
-from telebot import TeleBot, types
+from telebot import TeleBot
 from dotenv import load_dotenv
 import time
 import exceptions as ex
 import logging
-# from logging.handlers import StreamHandler
+import sys
 
 load_dotenv()
 
@@ -25,17 +25,25 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
+
 
 def check_tokens():
-    try:
-        if not (PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID):
-            raise ex.CheckTokensException
-    except ex.CheckTokensException as error:
-        logging.critical(error)
-        # завершить работу
+    """Docstring."""
+    if not (PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID):
+        logging.critical(
+            'В глобальном окружении отсутствует переменная '
+            'PRACTICUM_TOKEN, TELEGRAM_TOKEN или TELEGRAM_CHAT_ID!'
+        )
+        sys.exit()
 
 
 def send_message(bot, message):
+    """Отправка сообщения в Telegram."""
     try:
         bot.send_message(
             chat_id=TELEGRAM_CHAT_ID,
@@ -47,51 +55,56 @@ def send_message(bot, message):
 
 
 def get_api_answer(timestamp):
-    api_answer = requests.get(ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
-    return api_answer.json()
+    """Docstring."""
+    try:
+        api_answer = requests.get(ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
+        if api_answer.status_code != 200:
+            raise Exception(f'status code ответа {api_answer.status_code}')
+        return api_answer.json()
+    except Exception as error:
+        raise ex.EndpointException(error)
 
 
 def check_response(response):
+    """Docstring."""
+    if not isinstance(response, dict):
+        raise TypeError('формат ответа сервера должен быть классa dict')
     if 'homeworks' in response:
+        if not isinstance(response['homeworks'], list):
+            raise TypeError('класс homeworks должен быть list')
         if not response['homeworks']:
             raise ex.StatusNotChangedException
-    if 'code' in response:
+    elif 'code' in response:
         if response['code'] == 'UnknownError':
             raise ex.FormDateException
         elif response['code'] == 'not_authenticated':
             raise ex.NotAuthenticatedException
-        else:
-            raise Exception
+    else:
+        raise ex.NoKeyException('homeworks или code')
 
 
 def parse_status(homework):
+    """Docstring."""
     if 'status' in homework:
         status = homework.get('status')
         if status in HOMEWORK_VERDICTS:
             verdict = HOMEWORK_VERDICTS.get(status)
+            if 'homework_name' not in homework:
+                raise ex.NoKeyException('homework_name')
             homework_name = homework.get('homework_name')
             return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-        raise Exception
-    raise Exception
+        raise ex.UnexpectedStatusException(status)
+    raise ex.NoKeyException('status')
 
 
 def main():
     """Основная логика работы бота."""
-
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        filename='main.log',
-        level=logging.DEBUG,
-    )
-
     check_tokens()
 
     # Создаем объект класса бота
     bot = TeleBot(token=TELEGRAM_TOKEN)
-    timestamp = 0
-    # timestamp = int(time.time()) - RETRY_PERIOD
-
-    ...
+    timestamp = int(time.time()) - RETRY_PERIOD
+    previous_message = ''
 
     while True:
         try:
@@ -99,12 +112,19 @@ def main():
             check_response(response)
             message = parse_status(response.get('homeworks')[0])
             send_message(bot, message)
-            ...
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            if type(error) is ex.StatusNotChangedException:
+                logging.debug(message)
+            else:
+                if type(error) is not ex.SendingMessageException:
+                    if previous_message != message:
+                        send_message(bot, message)
+                        previous_message = message
+                logging.error(message)
+
+        time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
